70838a4426e4ec859dae2b91684d0410d68dece9
trantruong321 <tranvantruongtvtpro123@gmail.com>
2022-06-09 11:36:35 +0700

    fix migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('phone')->unique();
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->boolean('verified')->default(false);
             $table->unsignedBigInteger('refs')->nullable();
             $table->timestamp('email_verified_at')->nullable();
